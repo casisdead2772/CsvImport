075e347d73297068271c5d0016764488c9651d6f
Alexey Kostyukevich <78378059+casisdead2772@users.noreply.github.com>
2021-12-21 12:39:50 +0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -7,8 +7,6 @@
 
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
-// import "vue-snotify/styles/material.scss";
-// start the Stimulus application
 import './bootstrap';
 
 import Vue from 'vue'
